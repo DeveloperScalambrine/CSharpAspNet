bc4a294f6ce27ca2c3b648d7a2febb16c880e77f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-10-14 23:52:30 -0300

    add algorithm of Light Bulb

--- TypeOfDatas/Program.cs
+++ TypeOfDatas/Program.cs
@@ -1,22 +1,29 @@
-﻿class Program
+﻿
+namespace TypeOfDatas
 {
-  static void Main()
-  {
-    /* Input of data of two ways note that the method 
-    Parse we have an warning in the line twelve
-    */
-    Console.WriteLine("Type a date of type integer");
-    int Age = Convert.ToInt32(Console.ReadLine());
+	class Program
+	{
+		static void Main()
+		{
+			AlgorithmLightBulb test = new AlgorithmLightBulb();
+			test.Process();
+			
+			/* Input of data of two ways note that the method 
+			Parse we have an warning in the line twelve
+			*/
+			// Console.WriteLine("Type a date of type integer");
+			// int Age = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine("Type a date of type decimal");
-    decimal Average = decimal.Parse(Console.ReadLine());
-    
-    Console.WriteLine("Type a date of type boolean");
-    bool permission = Convert.ToBoolean(Console.ReadLine());
-    
-    Console.WriteLine("Type a date of type string");
-    string? Name = Console.ReadLine();
+			// Console.WriteLine("Type a date of type decimal");
+			// decimal Average = decimal.Parse(Console.ReadLine());
 
-  Console.WriteLine("Value inserted in INTEGER > " + Age + " Value inserted in DECIMAL > " + Average + " Value inserted in BOOLEAN > " + permission + " Value inserted in INTEGER > " + Name );
-  }
+			// Console.WriteLine("Type a date of type boolean");
+			// bool permission = Convert.ToBoolean(Console.ReadLine());
+
+			// Console.WriteLine("Type a date of type string");
+			// string? Name = Console.ReadLine();
+
+			// Console.WriteLine("Value inserted in INTEGER > " + Age + " Value inserted in DECIMAL > " + Average + " Value inserted in BOOLEAN > " + permission + " Value inserted in INTEGER > " + Name);
+		}
+	}
 }

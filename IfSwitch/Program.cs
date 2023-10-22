8653ee8b7a2b0941ac0158fa3717b8837d60dcb9
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-10-22 01:46:41 -0300

    done example with switch case

--- IfSwitch/Program.cs
+++ IfSwitch/Program.cs
@@ -4,6 +4,8 @@
 	{
 		static void Main(string[] args)
 		{
+			/* Example using structs of condition
+			 
 			string Situation = "";
 
 			Console.WriteLine("Type a value of note:");
@@ -18,7 +20,49 @@
 				Situation = "Reprovado";
 				Console.WriteLine(Situation);
 			}
+		*/
 
+			/* Example using struct switch case
+			Console.WriteLine("Digite um numero: ");
+			int number = Convert.ToInt32(Console.ReadLine());
+			switch (number)
+			{
+				case > 0:
+				Console.WriteLine("Maior que zero");
+				break;
+				case 0:
+				Console.WriteLine("Igual a zero");
+				break;
+				default:
+				Console.WriteLine("Menor que zero");
+				break;		
+			}
+		*/
+			Console.WriteLine("Type a count of polygon");
+			IdentifyPolygon(Convert.ToInt32(Console.ReadLine()));
+		}
+		public static string IdentifyPolygon(int sideCount)
+		{
+			string? name;
+			switch (sideCount)
+			{
+				case < 3:
+					name = "Não é um polígono";
+					break;
+				case 3:
+					name = "Triângulo";
+					break;
+				case 4:
+					name = "Quadrado";
+					break;
+				case 5:
+					name = "Pentágono";
+					break;
+				default:
+					name = "Polígono não identificado";
+					break;
+			}
+			return name;
 		}
 	}
 }

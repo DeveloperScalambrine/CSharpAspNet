ffe944601faeda3f49a567c8000a44a7fe88e115
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-10-23 08:58:55 -0300

    update

--- IfSwitch/Program.cs
+++ IfSwitch/Program.cs
@@ -37,7 +37,7 @@
 				Console.WriteLine("Menor que zero");
 				break;		
 			}
-		*/
+		
 			Console.WriteLine("Type a count of polygon");
 			IdentifyPolygon(Convert.ToInt32(Console.ReadLine()));
 		}
@@ -63,6 +63,63 @@
 					break;
 			}
 			return name;
+		*/
+
+			/* Example using while, do while and for and foreach
+
+			string? vacation;
+			vacation = Console.ReadLine();
+			bool vacationConverted = Convert.ToBoolean(vacation);
+			while (vacationConverted == false)
+			{
+				Console.WriteLine("Trabalhar");
+				break;				
+			}
+
+			int? count = 0;
+			while (count < 10)
+			{
+				Console.WriteLine("count" + count);
+				count+=1;
+			}
+			do
+			{
+				Console.WriteLine("count" + count);
+				count+=1;
+			} while (count < 10);
+			for (int count = 0; count < 3; count++)
+			{
+				Console.WriteLine("numero: " + count);
+			}
+
+			string[] names = new string[] { "Hulk", "Thor", "Loki" };
+			foreach (var name in names)
+			{
+				Console.WriteLine(name);
+			}
+		*/
+
+			// string[] teachers = new string[] { "Joel", "Tess", "Marlene" };
+			// string[] students = new string[] { "Ellie", "Joel", "Abby" };
+			// foreach (var teacher in teachers)
+			// {
+			// 	Console.WriteLine("Professor: " + teacher + ". Estudante:");
+			// 	foreach (var student in students)
+			// 	{
+			// 		if (teacher == student)
+			// 			break;
+			// 		Console.WriteLine(student);
+			// 	}
+			// }
+			int[] votes = new int[] { 1, 3, 5, 4, 1, 3, 1, 2 };
+			var count = 0;
+			foreach (var vote in votes)
+			{
+				if (vote > 3)
+					continue;
+				count++;
+			}
+			Console.WriteLine(count + " votos válidos ");
 		}
 	}
 }

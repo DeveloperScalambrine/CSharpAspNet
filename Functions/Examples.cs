8b78d9b1169aa23afeb1516fc89ca8ea35dc361d
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-11-07 12:04:41 -0300

    finish examples using functions

--- Functions/Examples.cs
+++ Functions/Examples.cs
@@ -2,7 +2,6 @@ namespace Functions
 {
   public class Person {
     public string? name = string.Empty;
-    // public double weight, height;
     public void  CalculatingPersonBmi() {
       double result, weight, height;
       Console.WriteLine("Type your Name: ");
@@ -15,16 +14,23 @@ namespace Functions
       Console.WriteLine($"{name} YOUR BMI IS: {result}");
     }
   
-    // public void CalculatingPersonBmi(double weight, double height) {
-    //   double result;
-    //   Console.WriteLine("Type your Name: ");
-    //   name = Console.ReadLine();
-    //   Console.WriteLine("Type your Weight");
-    //   weight = Convert.ToDouble(Console.ReadLine());
-    //   Console.WriteLine("Type your Height");
-    //   height = Convert.ToDouble(Console.ReadLine());
-    //   result = weight / (height * height);
-    //   Console.WriteLine($"{name} YOUR BMI IS: {result}");
-    // }
+    public double CalculatingPersonBmi(double weight, double height) {
+      return weight / (height * height);
+    }
+  
+    public int CalculateAgeByYear(int yearOfBirth) {
+      return DateTime.Now.Year - yearOfBirth;
+    }
+
+    public static string ValidateComingOfAge(int age) {
+      if (age >= 18)
+      {
+        return "Pode entrar";
+      }
+      else
+      {
+        return $"Não pode entrar voce tem {age} anos só é permitido maiores de 18 anos";
+      }
+    }
   }
 }
--- Functions/Program.cs
+++ Functions/Program.cs
@@ -4,17 +4,25 @@
   {
     static void Main(string[] args)
     {
-      string[] persons = new string[2];
+      // string[] persons = new string[2];
       Person carlos = new Person();
-      // carlos.CalculatingPersonBmi(carlos.weight, carlos.height);
-      for (int i = 0; i < persons.Length; i++)
-      {
+      // for (int i = 0; i < persons.Length; i++)
+      // {
         
-        carlos.CalculatingPersonBmi();
-        persons[i] = carlos.name;
+      //   carlos.CalculatingPersonBmi();
+      //   persons[i] = carlos.name;
+      // }
+      // Console.WriteLine($"Peoples registered: {string.Join(" , ", persons)}");
+      // double result = carlos.CalculatingPersonBmi(73,1.70);
+      // Console.WriteLine(Math.Round(result));
+      int increment = 0;
+      int age = carlos.CalculateAgeByYear(2011);
+      while (increment < 5)
+      increment+=1;
+      {
+      Console.WriteLine(Person.ValidateComingOfAge(age));
       }
-      Console.WriteLine($"Peoples registered: {string.Join(" , ", persons)}");
-      
+      Console.WriteLine(increment);
     }
   }
 }

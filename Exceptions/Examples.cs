ed78c1a840ab06e1051cafe82b7bdad6a54fadae
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-11-09 03:14:26 -0300

    Add examples how to use instruction try catch and finally

--- Exceptions/Examples.cs
+++ Exceptions/Examples.cs
@@ -1,9 +1,24 @@
+using System.Diagnostics;
 namespace Exceptions {
   
   public class Exceptions {
+    string methodName = string.Empty;
+    int res;
+    
     public int DivideNumbers(int numberOne, int numberTwo)
     {
-      return numberOne / numberTwo;
+      try 
+      {
+        res =  numberOne / numberTwo;
+        Console.WriteLine($"O valor da divisão é: {res}");     
+      }
+      catch (DivideByZeroException)
+      {
+        StackFrame sf = new StackFrame();
+        methodName = sf.GetMethod()!.Name; 
+        throw new DivideByZeroException($"Erro ao tentar realizar a divisão por um numero zero no método => {methodName} ");
+      }
+      return res;
     }
   }
 }
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -4,39 +4,39 @@
   {
     static void Main(string[] args)
     {
-      // try
-      // {
-      //   Exceptions exc = new Exceptions();
-      //   exc.DivideNumbers(10, 0);
-      // }
-      // catch (DivideByZeroException ex)
-      // {        
-      //   Console.WriteLine($"Usando a propriedade de exception Message: {ex.Message}");        
-      // }
-      // catch (Exception ex) {
-      //   Console.WriteLine($"Usando a propriedade de exception classe mais genérica Message: {ex.Message}");
-      // }
-      string[] chemicalProduct = new string[3];
-
       try
       {
-        chemicalProduct[0] = "Cálcio";
-        chemicalProduct[1] = "Zinco";
-        chemicalProduct[2] = "Hidrazina";
-        chemicalProduct[3] = "Anilina";
-        for (int i = 0; i < chemicalProduct.Length; i++)
-        {
-          Console.WriteLine(chemicalProduct[i]);
-        }
+        Exceptions exc = new Exceptions();
+        exc.DivideNumbers(10, 0);
       }
-      catch (IndexOutOfRangeException ex)
-      {
-        Console.WriteLine("Erro Específico, sabemos exatamente o motivo do erro." + ex.Message);
+      catch (DivideByZeroException ex)
+      {        
+        Console.WriteLine(ex.Message);        
       }
-      catch (Exception)
-      {
-        Console.WriteLine("Temos a mensagem, porém é um pouco incerto o que ocorreu.");
+      catch (Exception ex) {
+        // Console.WriteLine($"Usando a propriedade de exception classe mais genérica Message: {ex.Message}");
       }
+      // string[] chemicalProduct = new string[3];
+
+      // try
+      // {
+      //   chemicalProduct[0] = "Cálcio";
+      //   chemicalProduct[1] = "Zinco";
+      //   chemicalProduct[2] = "Hidrazina";
+      //   chemicalProduct[3] = "Anilina";
+      //   for (int i = 0; i < chemicalProduct.Length; i++)
+      //   {
+      //     Console.WriteLine(chemicalProduct[i]);
+      //   }
+      // }
+      // catch (IndexOutOfRangeException ex)
+      // {
+      //   Console.WriteLine("Erro Específico, sabemos exatamente o motivo do erro." + ex.Message);
+      // }
+      // catch (Exception)
+      // {
+      //   Console.WriteLine("Temos a mensagem, porém é um pouco incerto o que ocorreu.");
+      // }
 
     }
   }

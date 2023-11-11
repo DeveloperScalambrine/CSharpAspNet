77ac91a09755f4303bb44d3fc0c32e577bb96a3f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-11-11 04:59:41 -0300

    done examples using exceptions generics

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Exceptions
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using static Exceptions.Exceptions;
+
+namespace Exceptions
 {
   class Program
   {
@@ -6,17 +10,54 @@
     {
       try
       {
-        Exceptions exc = new Exceptions();
-        exc.DivideNumbers(10, 0);
+        // Criando um arquivo
+        // Exceptions CreateFileExceptions = new Exceptions();
+        // CreateFileExceptions.DivideNumbers(CreateFileExceptions.NumberOne, CreateFileExceptions.NumberTwo);
+
+        // verificar se existe um arquivo válido retorne o caminho e apague o arquivo
+        Exceptions FilesExceptions = new Exceptions();
+        string fulllPath = FilesExceptions.IsExistFile(FilesExceptions.source);
+        Console.WriteLine(fulllPath);
+
+        // Validação de nome vazio ou nulo, idade sendo do tipo int e se um nome consta na lista
+        // Person exc = new Person();
+        // string nameReturned = exc.ShowName("Henrique");
+        // object ageReturned = exc.ShowAge(37);
+        // // exc.ListPerson();
+        // bool IsUser = exc.VerifyName("Henrique");
+        // string result = (IsUser == true) ? "Processo finalizado" : "Processo não finalizado";
+        // Console.WriteLine(result);
       }
-      catch (DivideByZeroException ex)
-      {        
-        Console.WriteLine(ex.Message);        
+      catch (DivideByZeroException e)
+      {
+        Console.WriteLine(e.Message);
       }
-      catch (Exception ex) {
-        // Console.WriteLine($"Usando a propriedade de exception classe mais genérica Message: {ex.Message}");
+      catch (FileNotFoundException e)
+      {
+        Console.WriteLine(e.Message);
       }
-      // string[] chemicalProduct = new string[3];
+      catch (IndexOutOfRangeException e)
+      {
+        Console.WriteLine(e.Message);
+      }
+      catch (InvalidDataException e)
+      {
+        Console.WriteLine(e.Message);
+      }
+      catch (ArgumentNullException e)
+      {
+        Console.WriteLine(e.Message);
+      }
+      catch (ArgumentException e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+  }
+}
+      //   Console.WriteLine($"Usando a propriedade de exception classe mais genérica Message: {ex.Message}");
+      // }
+      // // string[] chemicalProduct = new string[3];
 
       // try
       // {
@@ -37,7 +78,3 @@
       // {
       //   Console.WriteLine("Temos a mensagem, porém é um pouco incerto o que ocorreu.");
       // }
-
-    }
-  }
-}

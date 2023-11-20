e7c0f7f6cfdeacf1983de3371502a7f2906173a4
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-11-20 04:14:27 -0300

    create folders dinamycs and using type date datetime

--- TypesAndDebug/Program.cs
+++ TypesAndDebug/Program.cs
@@ -1,16 +1,84 @@
-﻿namespace TypesAndDebug
+﻿using System.Diagnostics;
+
+namespace TypesAndDebug
 {
   class Program
   {
-    static void Main()
+    public static void Main()
     {
+      try
+      {
+        Console.WriteLine("Escolha a opção que deseja: ");
+        Console.WriteLine("1 > Criar Pasta");
+        Console.WriteLine("2 > Deletar Pasta");
+        Console.WriteLine("3 > Sair da aplicação");
+        int result = Convert.ToInt32(Console.ReadLine());
+        if (result == 1)
+        {
+          TypesAndDebug.createFolder();
+        }
+        else if (result == 2)
+        {
+          TypesAndDebug.deleteFolder();
+        } else if (result == 3)
+        {
+          Environment.Exit(0);
+        } 
+      }
+      catch (ArgumentException e)
+      {
+        Console.WriteLine(e.Message);
+      }
+      
+      // Criando mantendo ou apagando a pasta
+      // try
+      // {
+      //   TypesAndDebug.createFolder(); 
+      //   // TypesAndDebug.createFile();
+      // }
+      // catch (DirectoryNotFoundException e)
+      // {
+      //   e.Message.ToString();
+      // }
+      // catch (IOException e)
+      // {
+      //   Console.WriteLine(e.Message);
+      //   char res = Convert.ToChar(Console.ReadLine());
+      //   char answer = 's';
+
+      //   if (answer.Equals(res))
+      //   {
+      //     Directory.Delete(TypesAndDebug.caminhoPasta);
+      //     Console.WriteLine("Pasta apagada! ");
+      //   } else {
+      //     Console.WriteLine("Pasta mantida");
+      //   }
+      // }
+      // finally
+      // {
+      //  Console.WriteLine("Metodo executado 1(Criando a pasta) 2(Apagando a pasta) 3(Mantendo a pasta)");
+      // } 
+      // // int dt = DateTime.Now.AddYears(2).Year;
+      // int de = DateTime.Now.Year;
+      // int res = dt - de;
+      // int sum = res + de;
+      // Console.WriteLine(sum);
+      // Console.WriteLine("Digite quantos anos serão acrescentados: ");
+      // int sumYear = Convert.ToInt32(Console.ReadLine());
+      // Console.WriteLine(TypesAndDebug.SumYear(sumYear));
+      // TypesAndDebug subject = new TypesAndDebug();
+      // var dateOnly = date.Date;
+      // Console.WriteLine(dateOnly.ToString());
+      // System.Console.WriteLine(subject.Interpolation("carlos"));
+      //  System.Console.WriteLine(subject);
       // TypesAndDebug greet = new TypesAndDebug
       // {
       //   Name = "Carlos"
       // };
       //   Console.WriteLine(greet);      
-      string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com;email4@trybe.com;email5@trybe.com;email6@trybe.com";
-      string[] arrayEmails = emails.Split(";");
+      // string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com;email4@trybe.com;email5@trybe.com;email6@trybe.com";
+      // string[] arrayEmails = emails.Split(";");
+
     }
   }
 }
--- TypesAndDebug/TypesAndDebug.cs
+++ TypesAndDebug/TypesAndDebug.cs
@@ -1,15 +1,107 @@
 namespace TypesAndDebug
 {
-  class TypesAndDebug
+  public class TypesAndDebug
   {
     public string Name;
-    public override string ToString()
+    public static string nameFolder = string.Empty;
+    public static string caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/";
+
+    // public override string ToString()
+    // {
+    //   // string textOne = "Você está aprendendo sobre ";
+    //   // string textTwo = "Strings em C#, ";
+    //   // string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
+    //   // string res = string.Concat(textOne, textTwo, textThree);
+    //   // return $" {Name } {res}";
+    //   // var ff = DateTime.Now.AddYears(3);
+    //   // int dt = DateTime.Now.Year;
+    //   // return $"daqui tres anos estaremos em {ff} este anos estamos em {dt}";
+    // }
+
+    // public string  Interpolation(string name)
+    // {
+    //   string subject = $"Olá, {name}! Boas vindas ao sistema parceiro da Trybe!";
+    //   // Console.WriteLine(subject);
+    //   return subject;
+    // }
+    public static string SumYear(int sumYear)
     {
-      string textOne = "Você está aprendendo sobre ";
-      string textTwo = "Strings em C#, ";
-      string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
-      string res = string.Concat(textOne, textTwo, textThree);
-      return $" {Name } {res}";
+      int sumed = DateTime.Now.AddYears(sumYear).Year;
+      int currentYear = DateTime.Now.Year;
+      Console.WriteLine($"Estamos no ano de {currentYear}");
+      int differenceYears = Convert.ToInt32(sumed - currentYear);
+      int sumTotal = differenceYears + currentYear;
+      string result = $"Acrescentando os anos estamos em {sumTotal} ";
+      return result;
+    }
+
+    public static void started()
+    {
+      Program.Main();
+    }
+    public static void deleteFolder()
+    {
+      Console.WriteLine("Digite o nome da pasta que sera excluida");
+      nameFolder = Console.ReadLine();
+      caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/{nameFolder}";
+      if (!Directory.Exists(caminhoPasta))
+      {
+        createFolder();
+      }
+      Console.WriteLine("Deseja apagar a pasta");
+      char answer = Convert.ToChar(Console.ReadLine());
+      if (answer == 's')
+      {
+        Directory.Delete(caminhoPasta);
+        Console.WriteLine("Pasta apagada! ");
+      }
+      else if (answer == 'n')
+      {
+        Console.WriteLine("opçao de manter a pasta!!");
+        started();
+      }
+    }
+    public static void createFolder()
+    {
+      Console.WriteLine("Digite o nome da pasta");
+      nameFolder = Console.ReadLine();
+      caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/{nameFolder}";
+      if (Directory.Exists(caminhoPasta))
+      {
+        Console.WriteLine("Pasta ja existe não é possivel gravar Deseja apaga - la ? / s / n");
+        char res = Convert.ToChar(Console.ReadLine());
+        char answer = 's';
+        if (answer.Equals(res))
+        {
+          deleteFolder();
+        }
+        else
+        {
+
+          started();
+        }
+      }
+      Console.WriteLine("Deseja criar uma pasta");
+      char answern = Convert.ToChar(Console.ReadLine());
+      if (answern != 's')
+      {
+        throw new ArgumentException("Opção de não criar pasta e programa finalizado!!!");
+      }
+
+      Directory.CreateDirectory(caminhoPasta);
+      Console.WriteLine("Pasta criada! ");
+    }
+
+    public static void createFile()
+    {
+      string content = "Criado arquivo na pasta 2023 para bla bla bla";
+      string pathFile = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/2023/{content}.txt";
+      if (!File.Exists(pathFile))
+      {
+        throw new DirectoryNotFoundException("Pasta não encontrado tente outra pasta");
+      }
+      File.WriteAllText(pathFile, content);
+      Console.WriteLine("Arquivo criado com sucesso:");
     }
   }
 }

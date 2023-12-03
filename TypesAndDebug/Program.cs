4942e7ac61122383fcfca05f2e3673954f15427f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-12-03 20:28:20 -0300

    Done example of how create delete files and folders

--- TypesAndDebug/Program.cs
+++ TypesAndDebug/Program.cs
@@ -6,79 +6,62 @@ namespace TypesAndDebug
   {
     public static void Main()
     {
+      /* Criando uma instancia de TypesAndDebug para vermos a sobre escrita do metodo ToString
+
+        TypesAndDebug greet = new TypesAndDebug
+        {
+          Name = "Carlos"
+        };
+        Console.WriteLine(greet);
+      */
+      
+      /* Criando instancia de TypesAndDebug chamando o metodo estatico 
+         sumYear que tem como propriedade SumYears 
+         Console.WriteLine(TypesAndDebug.SumYear(TypesAndDebug.SumYears));
+      */
+
+      /* Setando as opções para criar deletar arquivos e pastas
       try
       {
-        Console.WriteLine("Escolha a opção que deseja: ");
-        Console.WriteLine("1 > Criar Pasta");
-        Console.WriteLine("2 > Deletar Pasta");
-        Console.WriteLine("3 > Sair da aplicação");
-        int result = Convert.ToInt32(Console.ReadLine());
-        if (result == 1)
-        {
-          TypesAndDebug.createFolder();
-        }
-        else if (result == 2)
-        {
-          TypesAndDebug.deleteFolder();
-        } else if (result == 3)
-        {
-          Environment.Exit(0);
-        } 
-      }
-      catch (ArgumentException e)
-      {
-        Console.WriteLine(e.Message);
-      }
-      
-      // Criando mantendo ou apagando a pasta
-      // try
-      // {
-      //   TypesAndDebug.createFolder(); 
-      //   // TypesAndDebug.createFile();
-      // }
-      // catch (DirectoryNotFoundException e)
-      // {
-      //   e.Message.ToString();
-      // }
-      // catch (IOException e)
-      // {
-      //   Console.WriteLine(e.Message);
-      //   char res = Convert.ToChar(Console.ReadLine());
-      //   char answer = 's';
+       Console.WriteLine("Escolha a opção que deseja: ");
+       Console.WriteLine("1 > Criar Pasta");
+       Console.WriteLine("2 > Criar Arquivo");
+       Console.WriteLine("3 > Deletar Pasta");
+       Console.WriteLine("4 > Deletar Arquivo");
+       Console.WriteLine("5 > Sair da aplicação");
+       int result = Convert.ToInt32(Console.ReadLine());
+       if (result == 1)
+       {
+         TypesAndDebug.createFolder();
+       }
+       else if (result == 2)
+       {
+         TypesAndDebug.createFile();
+       } else if (result == 3)
+       {
+         TypesAndDebug.deleteFolder();
+       } else if (result == 4) {
+         TypesAndDebug.deleteFile();
+       } else if (result == 5) {
+         Environment.Exit(0);
+       }
+     }
+     catch (ArgumentException e)
+     {
+       Console.WriteLine(e.Message);
+     }
+     catch (FileNotFoundException e)
+     {
+       Console.WriteLine(e.Message);
+     }
+     */
 
-      //   if (answer.Equals(res))
-      //   {
-      //     Directory.Delete(TypesAndDebug.caminhoPasta);
-      //     Console.WriteLine("Pasta apagada! ");
-      //   } else {
-      //     Console.WriteLine("Pasta mantida");
-      //   }
-      // }
-      // finally
-      // {
-      //  Console.WriteLine("Metodo executado 1(Criando a pasta) 2(Apagando a pasta) 3(Mantendo a pasta)");
-      // } 
-      // // int dt = DateTime.Now.AddYears(2).Year;
-      // int de = DateTime.Now.Year;
-      // int res = dt - de;
-      // int sum = res + de;
-      // Console.WriteLine(sum);
-      // Console.WriteLine("Digite quantos anos serão acrescentados: ");
-      // int sumYear = Convert.ToInt32(Console.ReadLine());
-      // Console.WriteLine(TypesAndDebug.SumYear(sumYear));
-      // TypesAndDebug subject = new TypesAndDebug();
-      // var dateOnly = date.Date;
-      // Console.WriteLine(dateOnly.ToString());
-      // System.Console.WriteLine(subject.Interpolation("carlos"));
-      //  System.Console.WriteLine(subject);
-      // TypesAndDebug greet = new TypesAndDebug
-      // {
-      //   Name = "Carlos"
-      // };
-      //   Console.WriteLine(greet);      
-      // string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com;email4@trybe.com;email5@trybe.com;email6@trybe.com";
-      // string[] arrayEmails = emails.Split(";");
-
+      /* Exemplo do uso do metodo Split para separarmos os emails com (;)
+       
+       string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com;email4@trybe.com;email5@trybe.com;email6@trybe.com";
+       string[] arrayEmails = emails.Split(";");
+       
+       */
     }
   }
 }
--- TypesAndDebug/TypesAndDebug.cs
+++ TypesAndDebug/TypesAndDebug.cs
@@ -2,48 +2,70 @@ namespace TypesAndDebug
 {
   public class TypesAndDebug
   {
-    public string Name;
+    public static int SumYears { get; set; }
+    public string? Name;
     public static string nameFolder = string.Empty;
-    public static string caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/";
+    public static string caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/TypesAndDebug/bin/Debug";
 
-    // public override string ToString()
-    // {
-    //   // string textOne = "Você está aprendendo sobre ";
-    //   // string textTwo = "Strings em C#, ";
-    //   // string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
-    //   // string res = string.Concat(textOne, textTwo, textThree);
-    //   // return $" {Name } {res}";
-    //   // var ff = DateTime.Now.AddYears(3);
-    //   // int dt = DateTime.Now.Year;
-    //   // return $"daqui tres anos estaremos em {ff} este anos estamos em {dt}";
-    // }
+    /// <summary>
+    /// Override in the method ToString
+    /// </summary>
+    /// <returns>The concatenation of three strings more field Name</returns>
+    public override string ToString()
+    {
+      string textOne = "Você está aprendendo sobre ";
+      string textTwo = "Strings em C#, ";
+      string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
+      string res = string.Concat(textOne, textTwo, textThree);
+      return $" { Name } { res }";
+    }
 
-    // public string  Interpolation(string name)
-    // {
-    //   string subject = $"Olá, {name}! Boas vindas ao sistema parceiro da Trybe!";
-    //   // Console.WriteLine(subject);
-    //   return subject;
-    // }
+    /// <summary>
+    /// this method make the sum of year current with the value into parameter
+    /// </summary>
+    /// <param name="sumYear"></param>
+    /// <returns> Year current more paramater sumYear</returns>
     public static string SumYear(int sumYear)
     {
+      Console.WriteLine("Digite quantos anos serão acrescentados:");
+      sumYear = Convert.ToInt16(Console.ReadLine());
       int sumed = DateTime.Now.AddYears(sumYear).Year;
       int currentYear = DateTime.Now.Year;
       Console.WriteLine($"Estamos no ano de {currentYear}");
-      int differenceYears = Convert.ToInt32(sumed - currentYear);
-      int sumTotal = differenceYears + currentYear;
-      string result = $"Acrescentando os anos estamos em {sumTotal} ";
+      // int differenceYears = Convert.ToInt32(sumed - currentYear);
+      // int sumTotal = differenceYears + currentYear;
+      string result = $"Acrescentando os anos estamos em {sumed} ";
       return result;
     }
 
+    /// <summary>
+    /// This method call the main method
+    /// </summary>
     public static void started()
+
     {
       Program.Main();
     }
+    
+    /// <summary>
+    /// This method verify wheter the file exist, if not exist call method Delete of class Directory
+    /// </summary>
+    ///
+    public static void deleteFile()
+    {
+      string pathFile = "/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/TypesAndDebug/bin/Debug/feito/teste.txt";
+      File.Delete(pathFile);
+      Console.WriteLine("Registro deletado");
+    }
+
+    /// <summary>
+    /// This method verify wheter the folder exist, if not exist call method Delete of class Directory
+    /// </summary>
     public static void deleteFolder()
     {
       Console.WriteLine("Digite o nome da pasta que sera excluida");
       nameFolder = Console.ReadLine();
-      caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/{nameFolder}";
+      caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/TypesAndDebug/bin/Debug/{nameFolder}";
       if (!Directory.Exists(caminhoPasta))
       {
         createFolder();
@@ -52,7 +74,7 @@ namespace TypesAndDebug
       char answer = Convert.ToChar(Console.ReadLine());
       if (answer == 's')
       {
-        Directory.Delete(caminhoPasta);
+        Directory.Delete(caminhoPasta, true);
         Console.WriteLine("Pasta apagada! ");
       }
       else if (answer == 'n')
@@ -61,11 +83,17 @@ namespace TypesAndDebug
         started();
       }
     }
+
+    /// <summary>
+    /// This method create a folder wheter not exist having how catch an 
+    /// ArgumentException
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
     public static void createFolder()
     {
       Console.WriteLine("Digite o nome da pasta");
       nameFolder = Console.ReadLine();
-      caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/{nameFolder}";
+      caminhoPasta = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/TypesAndDebug/bin/Debug/{nameFolder}";
       if (Directory.Exists(caminhoPasta))
       {
         Console.WriteLine("Pasta ja existe não é possivel gravar Deseja apaga - la ? / s / n");
@@ -77,7 +105,6 @@ namespace TypesAndDebug
         }
         else
         {
-
           started();
         }
       }
@@ -92,16 +119,29 @@ namespace TypesAndDebug
       Console.WriteLine("Pasta criada! ");
     }
 
+    /// <summary>
+    /// This method create a file and can catch for exceptions be a file not exist or folder not found
+    /// </summary>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    /// <exception cref="FileNotFoundException"></exception>
     public static void createFile()
     {
-      string content = "Criado arquivo na pasta 2023 para bla bla bla";
-      string pathFile = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/2023/{content}.txt";
-      if (!File.Exists(pathFile))
+      string content = "Criado arquivo na pasta feito, tendo como testemunha a gerlane chata";
+      string pathCurrent = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/TypesAndDebug/bin/Debug/feito";
+      string pathFile = $"/home/henrique/EstudosCSharp/CsharpAspNet/CSharpAspNet/TypesAndDebug/bin/Debug/feito/teste.txt";
+      
+      if (!Directory.Exists(pathCurrent))
       {
-        throw new DirectoryNotFoundException("Pasta não encontrado tente outra pasta");
+        throw new DirectoryNotFoundException("Pasta não encontrada");        
       }
-      File.WriteAllText(pathFile, content);
-      Console.WriteLine("Arquivo criado com sucesso:");
+        if (File.Exists(pathFile))
+        {
+          throw new FileNotFoundException("Arquivo ja existe");
+        }
+          File.WriteAllText(pathFile, content);
+          Console.WriteLine("Arquivo criado com sucesso:");
+          started();
+
+    }
     }
   }
-}

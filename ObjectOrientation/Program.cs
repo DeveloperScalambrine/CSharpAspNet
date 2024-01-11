b3a816366edfd0fd71612867d204e234eead9f92
Carlos Henrique <henrique.joaovictor@hotmail.com>
2024-01-10 21:53:11 -0300

    create a class Car without to use Composible

--- ObjectOrientation/Program.cs
+++ ObjectOrientation/Program.cs
@@ -4,14 +4,29 @@
   {
     static void Main(string[] args)
     {
-      var firstRocket = new Rocket("Apollo 11", 250000);
-      var secondRocket = new Rocket("Falcon 9", 300000);
-      Console.WriteLine(firstRocket);
-      Console.WriteLine(secondRocket);
+      // var firstRocket = new Rocket("Apollo 11", 250000);
+      // var secondRocket = new Rocket("Falcon 9", 300000);
+      // Console.WriteLine(firstRocket);
+      // Console.WriteLine(secondRocket);
     
-      
+      // var firstClient = new Client();
+      // Console.WriteLine(firstClient);
+      try
+      {
+         Car carCorolla = new Car();
+         carCorolla.StartEngine();
+         if (carCorolla.IsEngineStarted == true)
+         {
+            carCorolla.StopEngine();
+         } if (carCorolla.IsEngineStarted == false) {
+            carCorolla.StartEngine();
+         }    
+         carCorolla.Drive(12, 10);
+      }
+      catch (NullReferenceException e)
+      {        
+        Console.WriteLine(e.Message);
+      }
     }
-
   }
 }
-
